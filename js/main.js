2df068ac758700de266bbd19bdb42ead5f05231d
Lily-j17-source <liliana.1793@hotmail.com>
2020-12-07 22:25:08 -0600

    ADD Introduccion JS

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 console.log('hola koders')
+
+//variables
+//camelcase
+const NAME_KODER ='Lily'
+
+
+let name_koder = prompt('¿Cual es tu nombre?')
+
+console.log(name_koder)
+
+let age_koder = prompt('Cual es tu edad?')
+console.log(age_koder)
+
+let resultSuma = parseInt(age_koder) + 5
+console.log(resultSuma)
