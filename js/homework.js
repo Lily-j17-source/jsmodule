3ecba9924900385440d1712878651dd428aa92d3
Lily-j17-source <liliana.1793@hotmail.com>
2020-12-09 18:00:02 -0600

    ADD exercise-1

---
+++ js/homework.js
@@ -0,0 +1,23 @@
+//Exercise_1
+let score = parseInt(prompt('Cual es tu calificacion? (0-100) '))
+
+ 
+if(score >=90)
+{
+    console.log(`A`)
+} 
+else if(score >=80 && score < 90){
+    console.log(`B`)
+}
+else if(score >= 70 && score < 80){
+    console.log(`C`)
+} 
+else if(score >= 60 && score < 70){
+    console.log(`D`)
+}
+else if(score >= 50 && score < 60){
+    console.log(`e`)
+}
+    else {
+            console.log('F')
+}
