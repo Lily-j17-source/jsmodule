707f9308743ff384bbf6dbd5ffa03d49762d5551
Lily-j17-source <liliana.1793@hotmail.com>
2020-12-14 16:55:32 -0600

    ADD homework---Exercise2

--- js/homework.js
+++ js/homework.js
@@ -4,7 +4,7 @@
  * convertirlo a años caninos, e imprimir en consola 
  * calculateDogAge(2)
  * -> Poppy tiene 14 años caninos
- */
+ 
 let pideNameDog = prompt("Escribe el nombre de tu perrito")
 let pideAge = parseInt(prompt("Escribe la edad de tu perro en años humanos") ) 
 function calculateDogAge(nameDog,ageDog){
@@ -12,12 +12,27 @@ function calculateDogAge(nameDog,ageDog){
    console.log(`${nameDog} tiene ${calculaEdad} años caninos`)
 }
 calculateDogAge(pideNameDog,pideAge)
+*/
+
+
 /**
  * Crear una función que calcule el area y la circunferencia de un circulo
  * Tomar como parametro del radio del circulo 
  * calcGeometry(4)
  * -> l area del circulo es NN y la circunferencia es de NN
  */
+let radioCirculo = parseInt(prompt("Escribe el radio de la circunferencia") )
+function calcGeometry(radioCirculo){
+    const PI = 3.1416
+ let area= PI * radioCirculo * radioCirculo
+ let perimetro = 2 * PI * radioCirculo
+ console.log(`El área del circulo es: ${area} y la circunferencia es: ${perimetro}`)
+}
+
+calcGeometry(radioCirculo)
+
+
+
 /**
  * Crear una función que convierta los grados celsius en fahrenheit y viceversa
  * debe recibir la temparatura y si la temperatura esta en Celsius o Fahrenheit
