3092baf20c6a382644ed329a36f704c09fc0732f
Lily-j17-source <liliana.1793@hotmail.com>
2020-12-14 20:53:59 -0600

    ADD arrow-function-exercise1

---
+++ js/arrow_functions.js
@@ -0,0 +1,21 @@
+/*-----------ARROW FUNCTION----------------------*/
+/*const result = add(2,3)
+
+function add(a,b){
+    return a + b
+}
+
+const addArrow = (a,b) =>{
+    //sentencias
+    return a + b
+}
+
+let pideNameDog = prompt("Escribe el nombre de tu perrito")
+let pideAge = parseInt(prompt("Escribe la edad de tu perro en años humanos") ) 
+
+const edadPerrito = (pideNameDog,pideAge) => {
+    const dogYEar = 7* pideAge
+  return  dogYEar
+}
+ console.log(edadPerrito(pideNameDog,pideAge))
+*/
